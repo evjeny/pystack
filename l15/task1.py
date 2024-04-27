a7a3f5e354905923f36beb5f13c99932ed87bd02
evjeny <gkanafing@gmail.com>
2024-04-27 17:53:25 +0500

    add l15

---
+++ l15/task1.py
@@ -0,0 +1,13 @@
+class Transport:
+   def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+
+autobus = Transport(
+    name="Renaul Logan",
+    max_speed=180,
+    mileage=12
+)
+print(f"Название автомобиля: {autobus.name} Скорость: {autobus.max_speed} Пробег: {autobus.mileage}")
---
+++ l15/task2.py
@@ -0,0 +1,24 @@
+class Transport:
+   def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+ 
+   def seating_capacity(self, capacity):
+       return f"Вместимость одного автобуса {self.name}: {capacity} пассажиров"
+
+
+class Autobus(Transport):
+    def __init__(self, name, max_speed, mileage):
+        super().__init__(name, max_speed, mileage)
+    
+    def seating_capacity(self, capacity = 50):
+        return super().seating_capacity(capacity)
+
+
+autobus = Autobus(
+    name="Renaul Logan",
+    max_speed=180,
+    mileage=12
+)
+print(autobus.seating_capacity())
