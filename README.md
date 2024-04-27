66eefb576d92d9845f24b9ee0751bb1691021bf6
evjeny <gkanafing@gmail.com>
2024-04-27 16:49:40 +0500

    add l3

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # pystack
+
+## Урок 3. Ввод-вывод
+
+* [задание 1](./l3_input_output/task1.py)
+* [задание 2](./l3_input_output/task2.py)
---
+++ l3_input_output/task1.py
@@ -0,0 +1,5 @@
+animal_type = input("Введите вид животного: ")
+animal_age = input("Введите возраст животного: ")
+animal_name = input("Введите кличку животного: ")
+
+print(f"Это {animal_type} по кличке \"{animal_name}\". Возраст: {animal_age}.")
